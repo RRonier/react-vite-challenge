419fff64a56867b0d3ffb04e9b0f6560d0713a99
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-11-09 16:14:56 -0500

    adding axios calls as services

---
+++ src/services/products.service.js
@@ -0,0 +1,6 @@
+import { API } from "utils/API"
+
+export const getProducts = () => API.get('/')
+export const addProduct = () => API.get('/')
+export const editProduct = () => API.get('/:productId')
+export const deleteProduct = () => API.get('/:productId')

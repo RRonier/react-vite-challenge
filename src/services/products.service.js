6b88a2f76d79a1007827b5a8cbd392ed942d42f8
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-11-25 09:33:46 -0500

    changing products services

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,6 +1,7 @@
 import { API } from "../utils/API"
 
 export const getProductsService = () => API.get('/')
+export const selectProductService = (id) => API.get(`/${id}`)
 export const addProductService = (payload) => API.post('/', payload)
-export const editProductService = (id) => API.put(`/:${id}`)
-export const deleteProductService = (id) => API.delete(`/:${id}`)
+export const editProductService = (id) => API.put(`/${id}`)
+export const deleteProductService = (id) => API.delete(`/${id}`)
